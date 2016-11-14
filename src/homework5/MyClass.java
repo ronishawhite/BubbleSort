2d24e611dab621e6066be3057cfd4d13c588f4b6
ronishawhite <ronishawhite@gmail.com>
2016-11-14 15:11:10 -0500

    array before bubble sort
    array list before bubble sort method

--- src/homework5/MyClass.java
+++ src/homework5/MyClass.java
@@ -18,19 +18,8 @@ arr[j] = temp;
 } 
 public static void main(String[] args) { 
 int arr[] ={37,609,365,20,45,32,5}; 
-
+//array before bubble sort
 System.out.println("Array Before Bubble Sort"); 
 for(int i=0; i < arr.length; i++){ 
 System.out.print(arr[i] + " "); 
 } 
-System.out.println(); 
-
-bubbleSort(arr);//sorting array elements using bubble sort 
-
-System.out.println("Array After Bubble Sort"); 
-for(int i=0; i < arr.length; i++){ 
-System.out.print(arr[i] + " "); 
-} 
-
-} 
-}
