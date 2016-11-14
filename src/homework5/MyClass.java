2179ffbbf377f7ef673432e717252dea655d4cab
ronishawhite <ronishawhite@gmail.com>
2016-11-14 15:08:33 -0500

    my class
    class created

--- src/homework5/MyClass.java
+++ src/homework5/MyClass.java
@@ -11,3 +11,26 @@ temp = arr[j-1];
 arr[j-1] = arr[j]; 
 arr[j] = temp; 
 } 
+
+} 
+} 
+
+} 
+public static void main(String[] args) { 
+int arr[] ={37,609,365,20,45,32,5}; 
+
+System.out.println("Array Before Bubble Sort"); 
+for(int i=0; i < arr.length; i++){ 
+System.out.print(arr[i] + " "); 
+} 
+System.out.println(); 
+
+bubbleSort(arr);//sorting array elements using bubble sort 
+
+System.out.println("Array After Bubble Sort"); 
+for(int i=0; i < arr.length; i++){ 
+System.out.print(arr[i] + " "); 
+} 
+
+} 
+}
